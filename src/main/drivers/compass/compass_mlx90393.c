65245b20af552bae5f03cb46a2ae0ba57fca872b
Арианов Сергей Сергеевич <SSArianov@sberbank.ru>
2021-10-06 18:03:43 +0300

    Fixed Detect

--- src/main/drivers/compass/compass_mlx90393.c
+++ src/main/drivers/compass/compass_mlx90393.c
@@ -37,29 +37,29 @@
 #include "drivers/sensor.h"
 #include "drivers/compass/compass.h"
 
-#define MLX90393_MESURE_Z                           0b00001000
-#define MLX90393_MESURE_Y                           0b00000100
-#define MLX90393_MESURE_X                           0b00000010
-#define MLX90393_MESURE_T                           0b00000001
+#define MLX90393_MEASURE_Z                           0b00001000
+#define MLX90393_MEASURE_Y                           0b00000100
+#define MLX90393_MEASURE_X                           0b00000010
+#define MLX90393_MEASURE_T                           0b00000001
 
-#define MLX90393_START_BURST_MODE                   0b00010000 //uses with zyxt flags
-#define MLX90393_START_WAKE_UP_ON_CHANGE_MODE       0b00100000 //uses with zyxt flags
-#define MLX90393_START_MESUREMENT_MODE              0b00110000 //uses with zyxt flags
-#define MLX90393_READ_MESUREMENT                    0b01000000 //uses with zyxt flags
+#define MLX90393_START_BURST_MODE                    0b00010000 //uses with zyxt flags
+#define MLX90393_START_WAKE_UP_ON_CHANGE_MODE        0b00100000 //uses with zyxt flags
+#define MLX90393_START_MEASUREMENT_MODE              0b00110000 //uses with zyxt flags
+#define MLX90393_READ_MEASUREMENT                    0b01000000 //uses with zyxt flags
 
-#define MLX90393_RESET                              0b11110000
+#define MLX90393_RESET                               0b11110000
+#define MLX90393_RESET_FLAG			     0b00000100
 
 #define DETECTION_MAX_RETRY_COUNT   5
 
 static bool deviceDetect(magDev_t * mag)
 {
     for (int retryCount = 0; retryCount < DETECTION_MAX_RETRY_COUNT; retryCount++) {
-        delay(10);
-
-        uint8_t sig = 0;
-        bool ack = busRead(mag->busDev, MLX90393_START_MESUREMENT_MODE | MLX90393_MESURE_Z | MLX90393_MESURE_Y | MLX90393_MESURE_X | MLX90393_MESURE_T, &sig); //TODO: Start Single Measurement Mode zyxt
-
-        if (ack && ((sig & 0b00010000) == 0)) { //TODO: No error
+	uint8_t sig = 0;
+        bool ack = busRead(mag->busDev, MLX90393_RESET, &sig);
+        delay(20);
+ 
+        if (ack && ((sig & MLX90393_RESET_FLAG) == MLX90393_RESET_FLAG)) { // Check Reset Flag -> MLX90393
             return true;
         }
     }
@@ -70,13 +70,8 @@ static bool deviceDetect(magDev_t * mag)
 static bool mlx90393Init(magDev_t * mag)
 {
     uint8_t sig = 0;
-    bool ack;
-    UNUSED(ack);
 
-    ack = busRead(mag->busDev, MLX90393_RESET, &sig);
-    delay(10);
-
-    ack = busRead(mag->busDev, MLX90393_START_BURST_MODE | MLX90393_MESURE_Z | MLX90393_MESURE_Y | MLX90393_MESURE_X, &sig); //TODO: Start Single Measurement Mode zyx (if device was reset after deviceDetect)
+    busRead(mag->busDev, MLX90393_START_MEASUREMENT_MODE | MLX90393_MEASURE_Z | MLX90393_MEASURE_Y | MLX90393_MEASURE_X, &sig); //TODO: Start Single Measurement Mode zyx (if device was reset after deviceDetect)
     delay(20);
 
     return true;
@@ -87,7 +82,7 @@ static bool mlx90393Read(magDev_t * mag)
     bool ack = false;
     uint8_t buf[7] = {0};
 
-    ack = busReadBuf(mag->busDev, MLX90393_READ_MESUREMENT | MLX90393_MESURE_Z | MLX90393_MESURE_Y | MLX90393_MESURE_X, buf, 7); //TODO: read mesurement zyx
+    ack = busReadBuf(mag->busDev, MLX90393_READ_MEASUREMENT | MLX90393_MEASURE_Z | MLX90393_MEASURE_Y | MLX90393_MEASURE_X, buf, 7); //TODO: read mesurement zyx
 
     if (!ack) {
         return false;
